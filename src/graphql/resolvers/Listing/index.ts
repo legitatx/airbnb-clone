2c404e16cc4855aa734fcad6a6aa7ebda9be5706
legitatx <legitmodern.dev@gmail.com>
2021-01-26 11:16:38 -0500

    Start host listings impl + fix geocode bug

--- src/graphql/resolvers/Listing/index.ts
+++ src/graphql/resolvers/Listing/index.ts
@@ -1,7 +1,7 @@
 import { IResolvers } from "apollo-server-express";
 import { Request } from "express";
 import { ObjectId } from "mongodb";
-import { Database, Listing, User } from "../../../lib/types";
+import { Database, Listing, ListingType, User } from "../../../lib/types";
 import { authorize } from "../../../lib/utils";
 import { Google } from "../../../lib/api";
 import {
@@ -12,7 +12,26 @@ import {
   ListingsData,
   ListingsFilter,
   ListingsQuery,
+  HostListingArgs,
+  HostListingInput,
 } from "./types";
+
+const verifyHostListingInput = ({
+  title,
+  description,
+  type,
+  price,
+}: HostListingInput) => {
+  if (title.length > 100) {
+    throw new Error("Listing title must be under 100 characters.");
+  } else if (description.length > 5000) {
+    throw new Error("Listing description must be under 5000 characters.");
+  } else if (type !== ListingType.Apartment && type !== ListingType.House) {
+    throw new Error("Listing type must be either an apartment or house.");
+  } else if (price < 0) {
+    throw new Error("Price must be greater than 0.");
+  }
+};
 
 export const listingResolvers: IResolvers = {
   Query: {
@@ -44,7 +63,7 @@ export const listingResolvers: IResolvers = {
       { db }: { db: Database }
     ): Promise<ListingsData> => {
       try {
-        const query: any = {};
+        const query: ListingsQuery = {};
 
         const data: ListingsData = {
           region: null,
@@ -59,7 +78,7 @@ export const listingResolvers: IResolvers = {
             query.city = city;
           }
           if (admin) {
-            query.admin = city;
+            query.admin = admin;
           }
 
           if (country) {
@@ -93,6 +112,45 @@ export const listingResolvers: IResolvers = {
       } catch (error) {
         throw new Error(`Failed to query listings: ${error}`);
       }
+    },
+  },
+  Mutation: {
+    hostListing: async (
+      _root: undefined,
+      { input }: HostListingArgs,
+      { db, req }: { db: Database; req: Request }
+    ): Promise<Listing> => {
+      verifyHostListingInput(input);
+
+      let viewer = await authorize(db, req);
+      if (!viewer) {
+        throw new Error("Viewer cannot be found");
+      }
+
+      const { country, admin, city } = await Google.geocode(input.address);
+      if (!country || !admin || !city) {
+        throw new Error("Invalid address input");
+      }
+
+      const insertResult = await db.listings.insertOne({
+        _id: new ObjectId(),
+        ...input,
+        bookings: [],
+        bookingsIndex: {},
+        country,
+        admin,
+        city,
+        host: viewer._id,
+      });
+
+      const insertedListing: Listing = insertResult.ops[0];
+
+      await db.users.updateOne(
+        { _id: viewer._id },
+        { $push: { listings: insertedListing._id } }
+      );
+
+      return insertedListing;
     },
   },
   Listing: {
