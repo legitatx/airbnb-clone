95716249036ed5e5c2b5c41b0499fc1405878f38
legitatx <legitmodern.dev@gmail.com>
2021-01-28 00:30:04 -0500

    Prepare for SPA deployment

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-require("dotenv").config();
-
 import express, { Application } from "express";
 import bodyParser from "body-parser";
+import compression from "compression";
 import cookieParser from "cookie-parser";
 import { ApolloServer } from "apollo-server-express";
 import { connectDatabase } from "./database";
@@ -14,6 +13,10 @@ const start = async (app: Application) => {
 
   app.use(bodyParser.json({ limit: "2mb" }));
   app.use(cookieParser(process.env.SECRET));
+  app.use(compression());
+
+  app.use(express.static(`${__dirname}/client`));
+  app.get("/*", (_req, res) => res.sendFile(`${__dirname}/client/index.html`));
 
   const server = new ApolloServer({
     typeDefs,
