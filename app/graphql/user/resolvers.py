2c5ab7255e43a266a00ca7e03b35d809c83a6e37
legitatx <legitmodern.dev@gmail.com>
2021-02-01 20:46:38 -0500

    Reverse, ops of cursor are async not cursor itself

--- app/graphql/user/resolvers.py
+++ app/graphql/user/resolvers.py
@@ -9,8 +9,8 @@ from app.lib.models import User as UserModel
 class Query(graphene.ObjectType):
     user = graphene.Field(graphene.User, id=graphene.String(required=True))
 
-    async def resolve_user(parent: User, info, id):
-        user = await db.users.find_one({"_id": parent._id})
+    async def resolve_user(self, info, id):
+        user = await db.users.find_one({"_id": id})
         if user is None:
             raise HTTPException(
                 status_code=400, detail="Viewer could not be found"
@@ -20,10 +20,10 @@ class Query(graphene.ObjectType):
         if viewer:
             viewer = UserModel(**viewer)
 
-        if viewer._id == parent._id:
-            parent.authorized = True
+        if viewer._id == id:
+            self.user.authorized = True
 
-        return parent
+        return self.user
 
 
 class User(graphene.ObjectType):
@@ -39,12 +39,12 @@ class User(graphene.ObjectType):
             return None
 
         data = {"total": 0, "result": []}
-        cursor = await db.bookings.find({"_id": {"$in": parent.bookings}})
+        cursor = db.bookings.find({"_id": {"$in": parent.bookings}})
 
         cursor = cursor.skip((page - 1 if page > 0 else 0) * limit)
         cursor = cursor.limit(limit)
 
-        data["total"] = cursor.count()
-        data["result"] = cursor.to_list()
+        data["total"] = await cursor.count()
+        data["result"] = await cursor.to_list()
 
         return data
