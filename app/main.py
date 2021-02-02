330d729ac6d626fd9dace0308b74b4ddac33bd5c
legitatx <legitmodern.dev@gmail.com>
2021-02-01 20:47:45 -0500

    Fix schema

--- app/main.py
+++ app/main.py
@@ -4,6 +4,17 @@ import graphene
 from starlette.graphql import GraphQLApp
 from graphql.execution.executors.asyncio import AsyncioExecutor
 from app.database import load_database
+from app.graphql.booking.schema import Booking, Bookings
+from app.graphql.booking.resolvers import CreateBooking
+from app.graphql.listing.schema import (
+    Listing,
+    ListingFilter,
+    ListingType,
+    Listings,
+)
+from app.graphql.user.schema import User
+from app.graphql.user.resolvers import Query
+from app.graphql.viewer.schema import Viewer
 
 load_dotenv()
 db = load_database()
@@ -12,6 +23,21 @@ app = FastAPI()
 app.add_route(
     "/",
     GraphQLApp(
-        schema=graphene.Schema(query=Query), executor_class=AsyncioExecutor
+        #todo add root level query/mutation
+        schema=graphene.Schema(
+            query=Query,
+            types=[
+                Booking,
+                Bookings,
+                Listing,
+                ListingFilter,
+                ListingType,
+                Listings,
+                User,
+                Viewer,
+            ],
+            mutation=CreateBooking,
+        ),
+        executor_class=AsyncioExecutor,
     ),
 )
